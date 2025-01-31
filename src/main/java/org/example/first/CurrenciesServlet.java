d86b2a5c83aac6bb3b6862db37aca608e77c4b86
JavaThugNGG <ildarjava@mail.ru>
2025-01-31 14:18:25 +0300

    feat: Add POST /currencies/{code} endpoint.

--- src/main/java/org/example/first/CurrenciesServlet.java
+++ src/main/java/org/example/first/CurrenciesServlet.java
@@ -16,6 +16,7 @@ import java.sql.*;
 
 @WebServlet(value = "/currencies")
 public class CurrenciesServlet extends HttpServlet {
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         try {
             Class.forName("org.sqlite.JDBC");
@@ -59,4 +60,40 @@ public class CurrenciesServlet extends HttpServlet {
             throw new RuntimeException(e);
         }
     }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name");
+        String code = request.getParameter("code");
+        String sign = request.getParameter("sign");
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        File dbFile;
+        try {
+            dbFile = new File(SQLiteConnection.class.getClassLoader().getResource("database.db").toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:sqlite:" + dbFile.getAbsolutePath();
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "INSERT INTO Currencies (full_name, code, sign) " +
+                         "VALUES (?, ?, ?)";
+            try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setString(1, name);
+                stmt.setString(2, code);
+                stmt.setString(3, sign);
+                stmt.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
